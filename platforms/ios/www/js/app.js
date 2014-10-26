d7dc11ee0f4905109a4a36a37a397931fd3ff010
dabit3 <dabit3@gmail.com>
2014-10-25 20:18:11 -0500

    fixed gitignore, added ios

---
+++ platforms/ios/www/js/app.js
@@ -0,0 +1,23 @@
+var App = angular.module("App", ["ionic"]);
+
+App.service("FreshlyPressed", ["$http", FreshlyPressed]);
+
+App.controller("AppCtrl", ["$scope", "FreshlyPressed", AppCtrl]);
+
+function AppCtrl($scope, FreshlyPressed) {
+		$scope.posts = [];
+		$scope.refresh = function() {
+			FreshlyPressed.getBlogs($scope);
+		}
+}
+
+function FreshlyPressed($http) {
+	this.getBlogs = function($scope) {
+		$http.jsonp('https://public-api.wordpress.com/rest/v1/freshly-pressed?callback=JSON_CALLBACK')
+			.success(function(result) {
+					$scope.posts = result.posts;
+					$scope.$broadcast("scroll.refreshComplete");
+			})
+	}
+}
+
